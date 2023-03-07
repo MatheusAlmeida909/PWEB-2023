60ac3122f7b5f90913b36197e5899bb2f7167d47
MatheusAlmeida909 <almeidasmatheus909@gmail.com>
2023-03-07 10:46:11 -0300

    Ajuste de código js

--- scriptCalc.js
+++ scriptCalc.js
@@ -11,43 +11,36 @@ function calculate(idCalc) {
         case "add":
             result = num1 + num2;
 
-            document.getElementById("answer").value = result;
             break;
 
         case "subtract":
             result = num1 - num2;
 
-            document.getElementById("answer").value = result;
             break;
 
         case "multiply":
             result = num1 * num2;
 
-            document.getElementById("answer").value = result;
             break;
         
         case "divide":
             result = num1 / num2;
-
-            document.getElementById("answer").value = result;
+            
             break;
 
         case "square":
             result = num1 * num1;
-            extraResult = num2 * num2;
-            
-            if (num2 != null || num2 <= 0) {
+           
+            if (num1 != null || num1 <= 0) {
                 document.getElementById("answer").value = result;    
             } else {
-                if (num1 != null || num1 <= 0) {
-                    document.getElementById("answer").value = extraResult; //this line isn't working properly alongside line 45 "The specified value "NaN" cannot be parsed, or is out of range."
-                } else {
-                    document.getElementById("answer").value = result ;
-                } 
+                alert("Insira um numero válido para o calculo de potência (números maiores que 0 e não negativos).");
             }
             break;
     
         default:
             break;
     }
+
+    document.getElementById("answer").value = result;
 }
